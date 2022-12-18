fcd0c9306dcb5f0cb67815b0ec4837fc202bf576
mrytower05 <mtower@newrelic.com>
2022-12-18 07:36:28 -0700

    Changed data type from shorttext to integer

--- Mod_Dash/admin_panel/tasks/detailed_comments.sql
+++ Mod_Dash/admin_panel/tasks/detailed_comments.sql
@@ -1,10 +1,7 @@
 -- Add your SQL queries here.
 -- See SQL documentation: https://docs.airplane.dev/creating-tasks/sql
 SELECT 
-    customer_id,
-    username, 
     comment,
-    status,
     num_offenses
  FROM 
     comment_detail 
